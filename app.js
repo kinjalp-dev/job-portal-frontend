8ba2b5d3a2d76d75eb41d01b2b8f8731ead4b6bf
Kinjal Parmar <kinjalparmar.dev@gmail.com>
2025-11-24 21:51:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000/api/";
+const API_BASE = "http://127.0.0.1:8000/api";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Elements
@@ -251,4 +251,5 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
 
